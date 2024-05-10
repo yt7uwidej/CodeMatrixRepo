e00eab98c01ca56723312590e126d4dfea6150fe
yt7uwidej <gs1rrodders@hotmail.com>
2024-05-10 09:25:13 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const fibonacciRecursive = (n) => {
-  if (n <= 1) {
-    return n;
-  }
-  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
-};
+const average = (arr) => arr.reduce((a, v) => a + v, 0) / arr.length;
